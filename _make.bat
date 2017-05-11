63a9ecdcb8d18765afafd35701a3c208e0957d60
iszell <iszell@iszell.emacc.local>
2017-05-11 11:31:09 +0200

    Added demo to _make.bat

--- _make.bat
+++ _make.bat
@@ -1,7 +1,7 @@
 del *.d64
 del *.prg
 
-for %%i in ("loader_plus4_serial1bit" "loader_plus4_serial2bit" "loader_plus4_parallel" "loader_plus4_tcbm" "loader_drive_1bit1541" "loader_drive_2bit1541" "loader_drive_1bit1581" "loader_drive_2bit1581" "loader_drive_parallel1541" "loader_drive_tcbm" "loader_drive_gijoe" "decrunch" "IOLibV3Test" "IOLibV3ExoTest" "createtestpattern") do (
+for %%i in ("loader_plus4_serial1bit" "loader_plus4_serial2bit" "loader_plus4_parallel" "loader_plus4_tcbm" "loader_drive_1bit1541" "loader_drive_2bit1541" "loader_drive_1bit1581" "loader_drive_2bit1581" "loader_drive_parallel1541" "loader_drive_tcbm" "loader_drive_gijoe" "decrunch" "IOLibV3Test" "IOLibV3ExoTest" "createtestpattern" "demo") do (
 	bin\64tass --ascii -o %%i.prg %%i.asm	
 )
 
