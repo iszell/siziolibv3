c23b04bbd85384a5296889d0a2baec7ee01f528c
Siz <Siz@Siz-NB>
2020-06-03 18:19:47 +0200

    Add info display to demo

--- _make.bat
+++ _make.bat
@@ -2,11 +2,11 @@ del *.d64
 del *.prg
 
 for %%i in ("loader_plus4_serial1bit" "loader_plus4_serial2bit" "loader_plus4_parallel" "loader_plus4_tcbm" "loader_drive_1bit1541" "loader_drive_2bit1541" "loader_drive_1bit1581" "loader_drive_parallel1541" "loader_drive_tcbm" "loader_drive_gijoe" "decrunch" "IOLibV3Test" "IOLibV3ExoTest" "createtestpattern") do (
-	bin\64tass --ascii -o %%i.prg %%i.asm
+	bin\64tass --ascii -l %%i.lst -o %%i.prg %%i.asm
 )
 
-bin\64tass --ascii -o demo.tmp demo.asm
-exomizer\3.0.1\exomizer.exe sfx $100d -t 4 -o demo.prg demo.tmp
+bin\64tass --ascii -l demo.lst -o demo.tmp demo.asm
+exomizer\3.0.1\exomizer.exe sfx basic -t 4 -o demo.prg demo.tmp
 rem del demo.tmp
 bin\64tass --ascii -o bitmap1exo.tmp bitmap1exo.asm
 exomizer\3.0.1\exomizer.exe mem -f -o bitmap1exo.prg bitmap1exo.tmp
@@ -20,4 +20,4 @@ bin\cc1541 -f demo -w demo.prg -f iolibv3test -w iolibv3test.prg -f iolibv3exote
 bin\cc1541 -f demo -w demo.prg -f iolibv3test -w iolibv3test.prg -f iolibv3exotest -w iolibv3exotest.prg -f b1 -w bitmap1.bin -f e1 -w bitmap1exo.prg -f testpattern -w testpattern.bin -n iolibv3test -i io32a iolibv3test.d71
 bin\cc1541 -f demo -w demo.prg -f iolibv3test -w iolibv3test.prg -f iolibv3exotest -w iolibv3exotest.prg -f b1 -w bitmap1.bin -f e1 -w bitmap1exo.prg -f testpattern -w testpattern.bin -n iolibv3test -i io32d iolibv3test.d81
 
-start iolibv3test.d81
+start iolibv3test.d64
--- demo.asm
+++ demo.asm
@@ -2,6 +2,9 @@
 	.include	"basic_header.inc"
 	.include	"kernal-table.inc"
 
+charset	=	$4000
+bmp	=	$2000
+
 	jsr	primm
 	.text	"IOLibV3 demo by Siz (c) 2017.05.10"
 	.byte	14, 13, 0
@@ -11,6 +14,21 @@
 	bne	+
 	rts
 +	jsr	io_init
+
+	ldx	#0
+-	.for	i=0, i<4, i=i+1
+	lda	$d400+i*$100,x
+	sta	charset+i*$100,x
+	.next
+	inx
+	bne	-
+
+	lda	#0
+	sta	loopcnt
+	sta	loopcnt+1
+	sta	loopcnt+2
+	sta	loopcnt+3
+	sta	loopcnt+4
 
 	sei
 	sta	ted_ramen
@@ -43,16 +61,38 @@
 	lda	#$08
 	sta	ted_bitmapctrl
 	
-loadloop	ldx	#"b"
+loadloop
+	jsr	rstframe
+	ldx	#"b"
 	ldy	#"1"
 	jsr	io_load
+	inc	framerun
 	
 	jsr	clr
+	
+	jsr	incloop
+	
+	lda	#"R"
+	ldx	#"a"
+	ldy	#"w"
+	sta	modestr
+	stx	modestr+1
+	sty	modestr+2
+	jsr	prtstr
 
+	jsr	rstframe
 	ldx	#"e"
 	ldy	#"1"
 	jsr	io_decrunch
+	inc	framerun
 	jsr	clr
+	lda	#"E"
+	ldx	#"x"
+	ldy	#"o"
+	sta	modestr
+	stx	modestr+1
+	sty	modestr+2
+	jsr	prtstr
 	jmp	loadloop
 	
 clr	ldx	#0
@@ -89,6 +129,10 @@ irqline	= *+1
 	eor	#(203 ^ 2)
 	sta	irqline
 	
+	bpl	+
+	jsr	incframe
++
+	
 	lda	ted_irqsource
 	sta	ted_irqsource
 	
@@ -98,6 +142,132 @@ irqline	= *+1
 	tax
 	pla
 	rti
+
+rstframe
+	ldx	#0
+	stx	framecnt
+	stx	framecnt+1
+	stx	framecnt+2
+	stx	framecnt+3
+	stx	framecnt+4
+	inx
+	stx	framerun
+	rts
+
+incframe
+	ldy	#4
+-	ldx	framecnt,y
+	inx
+	cpx	#10
+	bne	+
+	lda	#0
+	sta	framecnt,y
+	dey
+	bpl	-
++	txa
+	sta	framecnt,y
+	rts
+	
+
+incloop	ldy	#4
+-	ldx	loopcnt,y
+	inx
+	cpx	#10
+	bne	+
+	lda	#0
+	sta	loopcnt,y
+	dey
+	bpl	-
++	txa
+	sta	loopcnt,y
+	ldx	#4
+-	lda	loopcnt,x
+	ora	#"0"
+	sta	loopstr,x
+	dex
+	bpl	-
+	rts
+
+prtstr	
+	ldx	#4
+-	lda	framecnt,x
+	ora	#"0"
+	sta	framestr,x
+	dex
+	bpl	-
+
+	lda	#0
+	sta	prtchar.dstaddr
+	lda	#(>bmp+24*320)
+	sta	prtchar.dstaddr+1
+	ldx	#0
+-	lda	string,x
+	bmi	+
+	cmp	#$40
+	bcc	++
+	and	#$1f
++	and	#$7f
++	jsr	prtchar
+	inx
+	cpx	#32
+	bne	-
+	rts
+
+prtchar	.block
+	sta	savea
+	stx	savex
+	lda	#0
+	sta	srcaddr+1
+	lda	savea
+	asl
+	rol	srcaddr+1
+	asl
+	rol	srcaddr+1
+	asl
+	rol	srcaddr+1
+	sta	srcaddr
+	lda	srcaddr+1
+	ora	#>charset
+	sta	srcaddr+1
+	ldx	#7
+srcaddr=*+1
+-	lda	charset,x
+dstaddr=*+1
+	sta	bmp+24*320,x
+	dex
+	bpl	-
+	lda	#8
+	clc
+	adc	dstaddr
+	sta	dstaddr
+	bcc	+
+	inc	dstaddr+1
++
+savex=*+1
+	ldx	#0
+savea=*+1
+	lda	#0
+	.bend
+	rts
+
+loopcnt	.fill	5
+		;012345
+framecnt
+	.fill	5
+framerun
+	.byte	0
+
+		;012345
+string	.text	"Loop: "
+		;6789abcdef0123
+loopstr	.text	"01234 Frames: "
+		;456789
+framestr
+	.text	"01234 "
+		;abc
+modestr	.text	"Raw"
+		;def
+	.text	"   "
 
 io_prtstatus = 1
 io_needmemdetect = 1
