5d1ef6fa51f162ca93f9a2e2f1180aba1e7f5ccd
Siz <Siz@Siz-NB>
2021-07-04 03:14:06 +0200

    Migrate to KickAss

--- IOLibV3ExoTest.asm
+++ IOLibV3ExoTest.asm
@@ -1,75 +1,79 @@
+#import	"plus4_basic_header.inc"
+#import "plus4_io_map.inc"
+#import "ted.inc"
+#import "plus4_kernal_table.inc"
 
-	.include	"basic_header.inc"
-	.include	"kernal-table.inc"
-	.include	"iolib_def.inc"
+	.encoding	"petscii_mixed"
 
 	jsr	primm
-	.text	"IOLibV3 Exomizer test by Siz"
 	.byte	13
 	.text	"(c) 2016.04.02"
 	.byte	14, 13, 0
 
-	jsr	io_detect
-;	lda	io_drivetyp
-	bne	+
+	jsr	iolib.detect
+	lda	iolib.drivedetect.io_drivetyp
+	bne	!+
 	rts
-+	jsr	io_init
+!:	jsr	iolib.init
 	jsr	primm
 	.byte	13
-	.null	"Testing loader: "
+	.text	"Testing loader: "
+	.byte	0
 	sei
 	lda	#<irq
 	ldx	#>irq
 	sta	$fffe
 	stx	$ffff
 	lda	#%00000010
-	sta	ted_irqmask
+	sta	ted.irqmask
 	lda	#220
-	sta	ted_rasterirqline
-	sta	ted_ramen
+	sta	ted.rasterirqline
+	sta	ted.ramen
+
 	cli
-	ldx	#"e"
-	ldy	#"x"
-	jsr	io_decrunch
-	sta	ted_romen
+	ldx	#'e'
+	ldy	#'1'
+	jsr	iolib.decrunch
+	sta	ted.romen
 	bcs	error
 	jsr	primm
 	.text	"done"
 	.byte	13, 0
 	lda	#$3b
-	sta	$ff06
+	sta	ted.ctrl1
 	lda	#$18
-	sta	$ff07
+	sta	ted.ctrl2
 	lda	#$18
-	sta	$ff12
+	sta	ted.bitmapctrl
 	lda	#$58
-	sta	$ff14
+	sta	ted.screenaddr
 	lda	$5bfe
-	sta	$ff15
+	sta	ted.background
 	lda	$5bff
-	sta	$ff16
+	sta	ted.color1
 	lda	#0
 	sta	$ff19
 	jmp	*
 	
-error	jsr	primm
+error:	jsr	primm
 	.text	"load error"
 	.byte	13, 0
 	rts
 	
-irq	inc	ted_border
+irq:	inc	ted.border
 	pha
-	lda	ted_irqsource
-	sta	ted_irqsource
+	lda	ted.irqsource
+	sta	ted.irqsource
 	pla
-	dec	ted_border
+	dec	ted.border
 	rti
 
-io_prtstatus = 1
-io_needmemdetect = 1
-io_needvideodetect = 1
-io_needsounddetect = 1
-io_needloader = 1
-io_needdecrunch = 1
-	.include	"iolib.inc"
-
+.namespace iolib {
+	#define	prtstatus
+	#define need_video_detect
+	#define need_memory_detect
+	#define need_sound_detect
+	#define need_loader
+	#define need_exodecrunch
+}
+#import "iolib.inc"
---
+++ testdisk.asm
@@ -0,0 +1,29 @@
+	.plugin "se.booze.kickass.CruncherPlugins"
+
+	.encoding	"petscii_mixed"
+
+	.disk [filename="testdisk.d64", name="IOLIBV3TEST", id="2021!" ]
+	{
+		[name="IOLIBV3TEST", type="prg", segments="iolibv3test" ],
+		[name="IOLIBV3EXOTEST", type="prg", segments="iolibv3exotest" ],
+		[name="TESTPATTERN", type="prg", segments="testdata" ],
+		[name="E1", type="prg", segments="exotestdata" ]
+	}
+
+	.segment iolibv3test []
+	* = $1001
+	.import c64 "IOLibV3Test.prg"
+
+	.segment iolibv3exotest []
+	* = $1001
+	.import c64 "IOLibV3ExoTest.prg"
+
+	.segment testdata []
+	* = $3000
+	.import c64 "testpattern.bin"
+
+	.segment exotestdata []
+	.modify ForwardMemExomizer($5800) {
+		* = $5800
+		.import c64 "dotctitle.bin"
+	}
