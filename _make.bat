d38e6ffe9d1482f171fc80a2d18c45af56dcc351
Imre Széll <siz@siz.hu>
2022-02-03 23:03:03 +0100

    Make init.prg self extracting

--- _make.bat
+++ _make.bat
@@ -1,7 +1,8 @@
 call bin\ka iolibv3test.asm
 call bin\ka iolibv3exotest.asm
 call bin\ka demo.asm
-call bin\ka init.asm
+call bin\ka -o init.tmp init.asm
+exomizer\3.1.1\exomizer sfx basic -t 4 -n -s "lda $ae pha" -f "pla sta $ae" -o init.prg init.tmp
 call bin\ka hwdetectplus4.asm
 call bin\ka hwdetect64.asm
 call bin\ka testdisk.asm
