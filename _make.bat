a035bb8df7f84f09a243e8dbcdab3416d4b80a30
Imre Széll <siz@siz.hu>
2022-03-06 18:46:02 +0100

    New testdisks for 1571 and 1581

--- _make.bat
+++ _make.bat
@@ -1,3 +1,9 @@
+@rem Cleanup
+del *.prg
+del *.d??
+del *.tmp
+
+@rem Make programs
 call bin\ka iolibv3test.asm
 call bin\ka iolibv3exotest.asm
 call bin\ka demo.asm
@@ -7,6 +13,24 @@ call bin\ka -o init.tmp init.asm
 exomizer\3.1.1\exomizer sfx basic -t 4 -n -s "lda $ae pha" -f "pla sta $ae" -o initquiet.prg init.tmp
 call bin\ka hwdetectplus4.asm
 call bin\ka hwdetect64.asm
-call bin\ka testdisk.asm
+call bin\ka testdata.asm
+exomizer\3.1.1\exomizer mem -f -o bitmapexodata.prg bitmap1.bin
+exomizer\3.1.1\exomizer mem -f -o exotestdata.prg dotctitle.bin
 
+@rem make disks
+call bin\ka testdisk.asm
+for %%i in (d64 d71 d81) do (
+bin\cc1541 -n "iolibv3test"^
+ -f demo -w demo.prg^
+ -f iolibv3test -w iolibv3test.prg^
+ -f iolibv3exotest -w iolibv3exotest.prg^
+ -f b1 -w bitmap1.bin^
+ -f e1 -w bitmapexodata.prg^
+ -f exotestdata -w exotestdata.prg^
+ -f "hwdetect plus/4" -w hwdetectplus4.prg^
+ -f "hwdetect 64" -w hwdetect64.prg^
+ testdisk.%%i
+)
+
+@rem start test disk
 start testdisk.d64
---
+++ testdata.asm
@@ -0,0 +1,6 @@
+	.plugin "se.booze.kickass.CruncherPlugins"
+
+	* = $3000
+	.for(var h=$30 ; h<$f0 ; h++) {
+		.for(var l=0 ; l<256 ; l++) .byte l
+	}
