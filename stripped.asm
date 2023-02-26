bee7990db2e253aa9fbf31722000650bbc6f60f6
Siz <siz@siz.hu>
2023-02-26 10:21:16 +0100

    Support for loading to specified address

--- stripped.asm
+++ stripped.asm
@@ -13,12 +13,14 @@ quit:
 !:  jmp $5555
 
 .namespace iolib {
-	.label	io_tcbmoffs	= $0700
-	.label	io_base		= $0701
+	.label	io_tcbmoffs	= $06ff
+	.label	io_base		= $0700
 	.label  io_bitbuff	= $b7
 	.label  io_loadptr  = $9e
-	#define need_loader
+	.label  io_loadflag = $9d
 }
+#define prtstatus
+#define need_loader
+#define need_loadflag
 #define skip_default_definitions
 #import "iolib.inc"
-
